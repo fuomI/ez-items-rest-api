9c8d02d993f7f01b23a0bf761f447d3fc490ae2c
Joni Jansson <joni.jansson@saunalahti.fi>
2022-05-06 13:10:29 +0300

    First route working as intended

--- api.js
+++ api.js
@@ -18,6 +18,20 @@ db.on('error', (error) => console.error(error));
 // If successful connection
 db.once('open', () => console.log('Connected to database'));
 
+// Set server to accept json
+app.use(express.json());
+
+// Routes
+
+// Get all items - /api/getall
+app.get('/api/getall', (req, res) => {
+    res.send('Hello World');
+});
+
+// Get one item with id - /api/:id
+// Create new item - /api/add
+// Update item (patch) - /api/update/:id
+// Delete item with id - /api/delete/:id
 
 // Start server
 app.listen(3000, () => console.log('Server running'));
